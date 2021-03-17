9026c52c00535f45284a12aa09577c4ebfc89ee8
Ivan Panchishin <rot1tweiler@gmail.com>
2021-03-18 04:08:19 +0500

    testing and other

---
+++ компиляторы/lama/Test2.s
@@ -0,0 +1,490 @@
+	.file "/vagrant/tmp/Test2.lama"
+
+	.stabs "/vagrant/tmp/Test2.lama",100,0,0,.Ltext
+
+	.globl	main
+
+	.data
+
+string_0:	.string	"Test2.lama"
+
+string_1:	.string	"\n"
+
+_init:	.int 0
+
+	.section custom_data,"aw",@progbits
+
+filler:	.fill	3, 4, 1
+
+	.stabs "l:S1",40,0,0,global_l
+
+global_l:	.int	1
+
+	.stabs "res1:S1",40,0,0,global_res1
+
+global_res1:	.int	1
+
+	.text
+
+.Ltext:
+
+	.stabs "data:t1=r1;0;4294967295;",128,0,0,0
+
+# PUBLIC ("main") / 
+
+# EXTERN ("Lfilter") / 
+
+# EXTERN ("Lremove") / 
+
+# EXTERN ("Lunzip") / 
+
+# EXTERN ("Lzip") / 
+
+# EXTERN ("LdeepFlatten") / 
+
+# EXTERN ("Lflatten") / 
+
+# EXTERN ("Lfind") / 
+
+# EXTERN ("Lassoc") / 
+
+# EXTERN ("Lreverse") / 
+
+# EXTERN ("Li__Infix_434343") / 
+
+# EXTERN ("Lmap") / 
+
+# EXTERN ("Liter") / 
+
+# EXTERN ("Lfoldr") / 
+
+# EXTERN ("Lfoldl") / 
+
+# EXTERN ("Lsize") / 
+
+# EXTERN ("LtagHash") / 
+
+# EXTERN ("LflatCompare") / 
+
+# EXTERN ("LcompareTags") / 
+
+# EXTERN ("LkindOf") / 
+
+# EXTERN ("Ltime") / 
+
+# EXTERN ("Lrandom") / 
+
+# EXTERN ("LdisableGC") / 
+
+# EXTERN ("LenableGC") / 
+
+# EXTERN ("Ls__Infix_37") / 
+
+# EXTERN ("Ls__Infix_47") / 
+
+# EXTERN ("Ls__Infix_42") / 
+
+# EXTERN ("Ls__Infix_45") / 
+
+# EXTERN ("Ls__Infix_43") / 
+
+# EXTERN ("Ls__Infix_62") / 
+
+# EXTERN ("Ls__Infix_6261") / 
+
+# EXTERN ("Ls__Infix_60") / 
+
+# EXTERN ("Ls__Infix_6061") / 
+
+# EXTERN ("Ls__Infix_3361") / 
+
+# EXTERN ("Ls__Infix_6161") / 
+
+# EXTERN ("Ls__Infix_3838") / 
+
+# EXTERN ("Ls__Infix_3333") / 
+
+# EXTERN ("Ls__Infix_58") / 
+
+# EXTERN ("Li__Infix_4343") / 
+
+# EXTERN ("Lcompare") / 
+
+# EXTERN ("Lwrite") / 
+
+# EXTERN ("Lread") / 
+
+# EXTERN ("Lfailure") / 
+
+# EXTERN ("Lfwrite") / 
+
+# EXTERN ("Lfread") / 
+
+# EXTERN ("Lfclose") / 
+
+# EXTERN ("Lfopen") / 
+
+# EXTERN ("Lfprintf") / 
+
+# EXTERN ("Lprintf") / 
+
+# EXTERN ("LmakeString") / 
+
+# EXTERN ("Lsprintf") / 
+
+# EXTERN ("LregexpMatch") / 
+
+# EXTERN ("Lregexp") / 
+
+# EXTERN ("Lsubstring") / 
+
+# EXTERN ("LmatchSubString") / 
+
+# EXTERN ("Lstringcat") / 
+
+# EXTERN ("LreadLine") / 
+
+# EXTERN ("Ltl") / 
+
+# EXTERN ("Lhd") / 
+
+# EXTERN ("Lsnd") / 
+
+# EXTERN ("Lfst") / 
+
+# EXTERN ("Lhash") / 
+
+# EXTERN ("Lclone") / 
+
+# EXTERN ("Llength") / 
+
+# EXTERN ("Lstring") / 
+
+# EXTERN ("LmakeArray") / 
+
+# EXTERN ("LstringInt") / 
+
+# EXTERN ("global_sysargs") / 
+
+# EXTERN ("Lsystem") / 
+
+# EXTERN ("LgetEnv") / 
+
+# EXTERN ("Lassert") / 
+
+# LABEL ("main") / 
+
+main:
+
+# BEGIN ("main", 2, 2, [], [], []) / 
+
+	.type main, @function
+
+	.cfi_startproc
+
+	movl	_init,	%eax
+	test	%eax,	%eax
+	jz	_continue
+	ret
+_continue:
+
+	movl	$1,	_init
+	pushl	%ebp
+	.cfi_def_cfa_offset	8
+
+	.cfi_offset 5, -8
+
+	movl	%esp,	%ebp
+	.cfi_def_cfa_register	5
+
+	subl	$Lmain_SIZE,	%esp
+	movl	%esp,	%edi
+	movl	$filler,	%esi
+	movl	$LSmain_SIZE,	%ecx
+	rep movsl	
+	call	__gc_init
+	pushl	12(%ebp)
+	pushl	8(%ebp)
+	call	set_args
+	addl	$8,	%esp
+	call	initList
+# SLABEL ("L1") / 
+
+L1:
+
+# CONST (4) / 
+
+	movl	$9,	%ebx
+# LINE (4) / 
+
+	.stabn 68,0,4,.L0
+
+.L0:
+
+# ST (Global ("l")) / 
+
+	movl	%ebx,	global_l
+# DROP / 
+
+# LD (Global ("l")) / 
+
+	movl	global_l,	%ebx
+# DUP / 
+
+	movl	%ebx,	%ecx
+# SLABEL ("L13") / 
+
+L13:
+
+# DUP / 
+
+	movl	%ecx,	%esi
+# TAG ("cons", 2) / 
+
+	movl	$1697575,	%edi
+	movl	$5,	-12(%ebp)
+	pushl	%ebx
+	pushl	%ecx
+	pushl	-12(%ebp)
+	pushl	%edi
+	pushl	%esi
+	call	Btag
+	addl	$12,	%esp
+	popl	%ecx
+	popl	%ebx
+	movl	%eax,	%esi
+# CJMP ("nz", "L11") / 
+
+	sarl	%esi
+	cmpl	$0,	%esi
+	jnz	L11
+# LABEL ("L12") / 
+
+L12:
+
+# DROP / 
+
+# JMP ("L9") / 
+
+	jmp	L9
+# LABEL ("L11") / 
+
+L11:
+
+# DUP / 
+
+	movl	%ecx,	%esi
+# CONST (0) / 
+
+	movl	$1,	%edi
+# CALL (".elem", 2, false) / 
+
+	pushl	%ebx
+	pushl	%ecx
+	pushl	%edi
+	pushl	%esi
+	call	Belem
+	addl	$8,	%esp
+	popl	%ecx
+	popl	%ebx
+	movl	%eax,	%esi
+# DROP / 
+
+# DUP / 
+
+	movl	%ecx,	%esi
+# CONST (1) / 
+
+	movl	$3,	%edi
+# CALL (".elem", 2, false) / 
+
+	pushl	%ebx
+	pushl	%ecx
+	pushl	%edi
+	pushl	%esi
+	call	Belem
+	addl	$8,	%esp
+	popl	%ecx
+	popl	%ebx
+	movl	%eax,	%esi
+# DROP / 
+
+# DROP / 
+
+# DUP / 
+
+	movl	%ebx,	%ecx
+# CONST (0) / 
+
+	movl	$1,	%esi
+# CALL (".elem", 2, false) / 
+
+	pushl	%ebx
+	pushl	%esi
+	pushl	%ecx
+	call	Belem
+	addl	$8,	%esp
+	popl	%ebx
+	movl	%eax,	%ecx
+# ST (Local (1)) / 
+
+	movl	%ecx,	-8(%ebp)
+# DROP / 
+
+# DUP / 
+
+	movl	%ebx,	%ecx
+# CONST (1) / 
+
+	movl	$3,	%esi
+# CALL (".elem", 2, false) / 
+
+	pushl	%ebx
+	pushl	%esi
+	pushl	%ecx
+	call	Belem
+	addl	$8,	%esp
+	popl	%ebx
+	movl	%eax,	%ecx
+# ST (Local (0)) / 
+
+	movl	%ecx,	-4(%ebp)
+# DROP / 
+
+# DROP / 
+
+# SLABEL ("L15") / 
+
+L15:
+
+# LD (Local (0)) / 
+
+	movl	-4(%ebp),	%ebx
+# LD (Local (1)) / 
+
+	movl	-8(%ebp),	%ecx
+# CONST (0) / 
+
+	movl	$1,	%esi
+# SEXP ("cons", 2) / 
+
+	movl	$1697575,	%edi
+	pushl	%ebx
+	pushl	%edi
+	pushl	%esi
+	pushl	%ecx
+	pushl	$7
+	call	Bsexp
+	addl	$16,	%esp
+	popl	%ebx
+	movl	%eax,	%ecx
+# CALL ("Li__Infix_434343", 2, false) / 
+
+	pushl	%ecx
+	pushl	%ebx
+	call	Li__Infix_434343
+	addl	$8,	%esp
+	movl	%eax,	%ebx
+# SLABEL ("L16") / 
+
+L16:
+
+# SLABEL ("L14") / 
+
+L14:
+
+# JMP ("L8") / 
+
+	jmp	L8
+# LABEL ("L9") / 
+
+L9:
+
+# FAIL ((6, 16), true) / 
+
+	pushl	$33
+	pushl	$13
+	pushl	$string_0
+	pushl	%ebx
+	call	Bmatch_failure
+	addl	$16,	%esp
+# JMP ("L8") / 
+
+	jmp	L8
+# LABEL ("L8") / 
+
+L8:
+
+# LINE (6) / 
+
+	.stabn 68,0,6,.L1
+
+.L1:
+
+# ST (Global ("res1")) / 
+
+	movl	%ebx,	global_res1
+# DROP / 
+
+# LINE (10) / 
+
+	.stabn 68,0,10,.L2
+
+.L2:
+
+# LD (Global ("res1")) / 
+
+	movl	global_res1,	%ebx
+# CALL ("Lstring", 1, false) / 
+
+	pushl	%ebx
+	call	Lstring
+	addl	$4,	%esp
+	movl	%eax,	%ebx
+# STRING ("\\n") / 
+
+	movl	$string_1,	%ecx
+	pushl	%ebx
+	pushl	%ecx
+	call	Bstring
+	addl	$4,	%esp
+	popl	%ebx
+	movl	%eax,	%ecx
+# CALL ("Li__Infix_4343", 2, false) / 
+
+	pushl	%ecx
+	pushl	%ebx
+	call	Li__Infix_4343
+	addl	$8,	%esp
+	movl	%eax,	%ebx
+# CALL ("Lprintf", 1, false) / 
+
+	pushl	%ebx
+	call	Lprintf
+	addl	$4,	%esp
+	movl	%eax,	%ebx
+# SLABEL ("L2") / 
+
+L2:
+
+# END / 
+
+	movl	%ebx,	%eax
+Lmain_epilogue:
+
+	movl	%ebp,	%esp
+	popl	%ebp
+	xorl	%eax,	%eax
+	.cfi_restore	5
+
+	.cfi_def_cfa	4, 4
+
+	ret
+	.cfi_endproc
+
+	.set	Lmain_SIZE,	12
+
+	.set	LSmain_SIZE,	3
+
+	.size main, .-main
+
